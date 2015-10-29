265f5c9945e6b650bf7ca32b7c59b85e0d7d24a8
Renjie Sun <sunrenjie6@gmail.com>
2015-10-29 15:58:51 +0800

    [ch02] Added comments along with a little fix
    The code may be fine. But there is NO inline comments, which makes it
    very hard to decipher. Some explanatory comments are added.
    
    In addition, one little fix is made: the input data path is specified as
    the 1st argument, instead of being hard-coded. Now we could run the code
    as:
    spark-submit --master spark://localhost.localdomain:7077 \
      --executor-memory 512M --class com.cloudera.datascience.intro.RunIntro \
      target/ch02-intro-1.0.2.jar hdfs://localhost:9000/user/r/linkage

--- ch02-intro/src/main/scala/com/cloudera/datascience/intro/RunIntro.scala
+++ ch02-intro/src/main/scala/com/cloudera/datascience/intro/RunIntro.scala
@@ -18,8 +18,7 @@ case class Scored(md: MatchData, score: Double)
 object RunIntro extends Serializable {
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext(new SparkConf().setAppName("Intro"))
-   
-    val rawblocks = sc.textFile("hdfs:///user/ds/linkage")
+    val rawblocks = sc.textFile(args(0))
     def isHeader(line: String) = line.contains("id_1")
     
     val noheader = rawblocks.filter(x => !isHeader(x))
@@ -41,11 +40,14 @@ object RunIntro extends Serializable {
 
     val matchCounts = parsed.map(md => md.matched).countByValue()
     val matchCountsSeq = matchCounts.toSeq
+    // Print counts of matched(true) and unmatched(false) records.
     matchCountsSeq.sortBy(_._2).reverse.foreach(println)
 
     val stats = (0 until 9).map(i => {
       parsed.map(_.scores(i)).filter(!_.isNaN).stats()
     })
+    // For each of the 9 columns of scores, filter out NaN, feed the remaining
+    // to stats(). Then print them all, one record per line.
     stats.foreach(println)
 
     val nasRDD = parsed.map(md => {
@@ -54,8 +56,15 @@ object RunIntro extends Serializable {
     val reduced = nasRDD.reduce((n1, n2) => {
       n1.zip(n2).map { case (a, b) => a.merge(b) }
     })
+    // Print NAStatCounter for all the columns of scores, one record per line.
+    // Since NAStatCounter is largely a wrapper around StatCounter, the results
+    // shall agree with the above output.
     reduced.foreach(println)
 
+    // For all the score columns, compute StatCounters for the matched and the
+    // unmatched records, for which calculate the sum of the number of NaN
+    // data points and difference of means. Finally print them, one column per
+    // line.
     val statsm = statsWithMissing(parsed.filter(_.matched).map(_.scores))
     val statsn = statsWithMissing(parsed.filter(!_.matched).map(_.scores))
     statsm.zip(statsn).map { case(m, n) =>
@@ -68,13 +77,26 @@ object RunIntro extends Serializable {
       Scored(md, score)
     })
 
+    // For all records, compute a special score, which is sum of the scores at
+    // 3rd, 6th, 7th, 8th, 9th columns. Filter the score by >=4.0, count and
+    // print the number of matched and unmatched records.
     ct.filter(s => s.score >= 4.0).
       map(s => s.md.matched).countByValue().foreach(println)
+    // Set threshold to >=2.0, do it again.
     ct.filter(s => s.score >= 2.0).
       map(s => s.md.matched).countByValue().foreach(println)
   }
 
   def statsWithMissing(rdd: RDD[Array[Double]]): Array[NAStatCounter] = {
+    // An efficient way of computing NAStatCounter for all data columns
+    // based on mapPartitions().
+    // 1. Within every partition, construct an array of NAStatCounter (of
+    //    the same dimension as that of the array in rdd), with ith element
+    //    result of adding each values in the ith element in all elements in
+    //    the partition. The resulting rdd will has one element (of type
+    //    Array[NAStatCounter]) in each partition.
+    // 2. Reduce by merging all NAStatCounter arrays and return the final
+    //    Array[NAStatCounter].
     val nastats = rdd.mapPartitions((iter: Iterator[Array[Double]]) => {
       // Has to protect against empty iterators (e.g., small input); see also:
       // http://stackoverflow.com/questions/31605440/how-to-use-mappartitions-in-scala
